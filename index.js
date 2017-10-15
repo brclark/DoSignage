c523f2a881f8376695ce70fd57501913262a7bd7
Kyle Joseph <kyle.sowards@gmail.com>
2017-10-14 19:12:25 -0500

    switch node logging out of console to winston

--- index.js
+++ index.js
@@ -2,21 +2,21 @@ const http = require('http')
 const path = require('path')
 const phantom = require('phantomjs-prebuilt')
 const { spawn } = require('child_process')
+const winston = require('winston')
 const port = 3000
 
 const requestHandler = (request, response) => {
-  console.log(request.url)
-  response.end('Hello Node.js Server!')
+  winston.log('info', 'node-app', { log: request.url })
 }
 
 const server = http.createServer(requestHandler)
 
 server.listen(port, (err) => {
   if (err) {
-    return console.log('something bad happened', err)
+    return winston.log('error', 'something bad happened', {error: err})
   }
 
-  console.log(`server is listening on ${port}`)
+  return winston.log('info', 'node-app', {info: `server is listening on ${port}`})
 })
 
 const p = spawn(phantom.path, ['./phantom-job.js'])
@@ -24,14 +24,20 @@ p.stdout.setEncoding('utf8')
 p.stderr.setEncoding('utf8')
 
 p.stdout.on('data', (data) => {
-  console.log("stdout:", data )
+  winston.log('info', 'phantom-js', {
+    stdout: `${data}`
+  })
 })
 
 p.stderr.on('data', (data) => {
-  console.log("stderr:", data )
+  winston.log('error', 'phantom-js', {
+    stderr: `${data}`
+  })
 })
 
 p.on('error', (data) => {
-  console.log("err:",  data )
+  winston.log('error', 'spawn', {
+    child_process_error: `${data}`
+  })
 })
 
