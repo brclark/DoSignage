64f591ce0a2f9a970ad6e4914124fd438a1b6109
Kyle Joseph <kyle.sowards@gmail.com>
2017-10-19 22:54:42 -0500

    getting rid of semi-colons because semi-colons are whack, dog

--- phantom-job.js
+++ phantom-job.js
@@ -1,17 +1,18 @@
-var system = require('system');
-var fs = require('fs');
-var page = require('webpage').create();
+var system = require('system')
+var fs = require('fs')
+var page = require('webpage').create()
 var url = "http://do314.com"
-var path = './output.txt';
+var path = './output.html'
 
 page.open( url, function( status ) {
 
 	if(status === "success") {
     fs.touch(path);
     fs.write(path, page.content, 'w');
-    system.stdout.write( "content written to output.txt" )
+    system.stdout.write( "content written to output.html" )
   }
 
+  system.stdout.write( "phantom job complete. exiting." )
 	phantom.exit();
-});
+})
 
