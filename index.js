b2bcc829e43a9f8da5dd6ddb238a3f7c55334fe9
Ben Clark <brclark15@gmail.com>
2017-10-24 14:10:23 -0500

    Add ability to schedule the phantom webscrape using node-cron
    Phantom currently scrapes the url once every minute

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const { spawn } = require('child_process')
 const winston = require('winston')
 const port = 3000
 
+var cron = require('node-cron')
+
 const requestHandler = (request, response) => {
   winston.log('info', 'node-app', { log: request.url })
 }
@@ -19,19 +21,35 @@ server.listen(port, (err) => {
   return winston.log('info', 'node-app', {info: `server is listening on ${port}`})
 })
 
-const p = spawn(phantom.path, ['./phantom-job.js'])
-p.stdout.setEncoding('utf8')
-p.stderr.setEncoding('utf8')
+function parse_events(code) {
+  console.log('Created json with events ret: ' + code)
+  console.log('Time to parse json')
+}
 
-p.stdout.on('data', (data) => {
-  winston.log('info', 'phantom-js', { stdout: `${data}` })
-})
+function phantom_web_scrape() {
+  var p = spawn(phantom.path, ['./phantom-job.js']);
 
-p.stderr.on('data', (data) => {
-  winston.log('error', 'phantom-js', { stderr: `${data}` })
-})
+  winston.log('info', 'node-app', {info: `${phantom.path}` })
+  p.stdout.setEncoding('utf8')
+  p.stderr.setEncoding('utf8')
 
-p.on('error', (data) => {
-  winston.log('error', 'spawn', { child_process_error: `${data}` })
-})
+  p.stdout.on('data', (data) => {
+     winston.log('info', 'phantom-js', { stdout: `${data}` })
+  })
 
+  p.stderr.on('data', (data) => {
+    winston.log('error', 'phantom-js', { stderr: `${data}` })
+  })
+
+  p.on('error', (data) => {
+    winston.log('error', 'spawn', { child_process_error: `${data}` })
+  })
+
+  p.on('close', (code) => {
+    parse_events(code)
+  })
+}
+
+//phantom_web_scrape()
+// Currently set to scrape once every minute
+cron.schedule('* * * * *', phantom_web_scrape)
